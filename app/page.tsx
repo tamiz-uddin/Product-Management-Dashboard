8508dc85be50d04748813149d302e485111a564e
tamiz-uddin <tamizuddin.tr@gmail.com>
2025-12-25 18:41:18 +0600

    modified page.tsx in app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import HomePage from '@/components/Home/HomePageContent';
+import { Spin } from 'antd';
 import  { Suspense } from 'react';
 
 const page = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Spin fullscreen size="large"/>}>
       <HomePage />
     </Suspense>
   );
